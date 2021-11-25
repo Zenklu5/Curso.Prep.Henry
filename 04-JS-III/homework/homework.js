2c80f618e5f2dcc33e36885c8c21c5ecaa3282b2
Zenklu <tomaszenklusen5@gmail.com>
2021-11-25 00:10:19 -0300

    87

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -118,14 +118,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador = 0;
+  for (var i = 0; i < arreglo.length ; i++) {
+    if(arreglo[i] > 19){
+      contador ++
+    }
+  }
+  return contador
 }
 
 
@@ -134,7 +145,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia===1|numeroDeDia===7)
+  return "Es fin de semana"
+  else
+  return "Es dia Laboral"
 } 
 
 
@@ -142,7 +156,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true
+  }
+  return false
 }
 
 
@@ -150,7 +168,14 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var tof = 0
+  for (var i=0; arreglo.length > i; i ++){
+    if (arreglo[i]===arreglo[i+1]) tof++
+  }
+  if (tof>0) 
+  return true
+  else
+  return false
 } 
 
 
